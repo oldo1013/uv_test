a667f314858b68d891ccbd1dc97abeb6919592c0
KrisKlement <test@test>
2025-04-11 12:51:06 +0100

    rewrite for allure reporting

--- test/test_my_new_project.py
+++ test/test_my_new_project.py
@@ -1,4 +1,5 @@
 import pathlib
+import allure
 
 from src.my_new_project import get_name, get_year, get_town
 
@@ -8,18 +9,24 @@ yearfile = parent / 'src' / 'year.txt'
 townfile = parent / 'src' / 'my_new_project' / 'town.txt'
 
 
+@allure.title('Testing name')
+@allure.description('Testing name in the file')
 def test_name():
     with open(namefile) as f:
         name = f.readline()
     assert name == get_name(), 'Names are not equal'
 
 
+@allure.title('Testing year')
+@allure.description('Testing year in the file')
 def test_year():
     with open(yearfile) as f:
         year = f.readline()
     assert year == get_name(), 'Years are not equal'
 
 
+@allure.title('Testing town')
+@allure.description('Testing town in the file')
 def test_town():
     with open(townfile) as f:
         town = f.readline()
